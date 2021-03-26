9404b42978c9ad00cadde1aea9152f930f8de9a5
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-26 14:07:41 +0530

    Omk

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -6,6 +6,7 @@ import re
 import time
 
 from telethon import Button, custom, events
+from telegram import ParseMode
 
 from . import CMD_LIST, catalive
 
@@ -140,9 +141,9 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
 
             buttons = [custom.Button.inline("🔐 SHOW", data=f"secret_{timestamp}")]
             result = builder.article(
-                title="secret message",
-                text=f"<b>📩 Secret Msg</b> for <b>{sandy}</b>. Only he/she can open it..",
-                buttons=buttons,
+                title="Secret Message",
+                text=f"**📩 Secret Msg** for **{sandy}**. Only he/she can open it..",
+                buttons=buttons, parse_mode=ParseMode.Markdown,
             )
             await event.answer([result] if result else None)
             if jsondata:
